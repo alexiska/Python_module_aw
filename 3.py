f17684acb83c33dd1a0662ce0627c7bfef653039
Alexiska <alexiska@kth.se>
2021-06-14 10:58:30 +0200

    initial commit uploading python files

---
+++ 3.py
@@ -0,0 +1,33 @@
+# -- a --
+fname = 'Alexis'
+lname = "Kan"
+course = 'DE'
+no_candidates = 10
+
+print(f"My name is \n{fname}")
+print(f"My last name is \n{lname}")
+print(f"I am participating in the course \n{course}")
+print(f"There are \n{no_candidates}\ncandidates taking the\n{course}\ncourse.")
+
+# -- b --
+fname = 'Tripp'
+lname = "Trapp"
+course = 'DS'
+no_candidates = 100
+
+print(f"My name is \n{fname}")
+print(f"My last name is \n{lname}")
+print(f"I am participating in the course \n{course}")
+print(f"There are \n{no_candidates}\ncandidates taking the\n{course}\ncourse.")
+
+
+# -- c --
+fname = 'Tripp'
+lname = "Trapp"
+course = 'DS'
+no_candidates = 100
+
+print(f"My name is {fname}", end=' ')
+print(f"My last name is {lname}", end=' ')
+print(f"I am participating in the course {course}", end=' ')
+print(f"There are {no_candidates} candidates taking the {course} course.", end=' ')
