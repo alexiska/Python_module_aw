75ff990f5028595327d7366fd95e2b84aadeddce
Alexiska <alexiska@kth.se>
2021-06-14 14:05:55 +0200

    New exercises added

---
+++ ex042/2.py
@@ -0,0 +1,20 @@
+# -- a --
+fname = 'TRiPP'
+lname = "TrApp"
+course = 'TrUll'
+no_candidates = 10
+
+print(f"My name is \n{fname}\n. My last name is \n{lname}\n \
+I am participating in the course \n{course}\n \
+There are \n{no_candidates}\ncandidates taking the\n{course}\ncourse.")
+
+# -- b & c--
+fname = 'TRiPP'
+lname = "TrApp"
+course = 'TrUll'
+no_candidates = 10
+
+print(f"My name is \n{fname.capitalize()}\n. My last name is \n{lname.capitalize()}\n \
+I am participating in the course \n{course}\n \
+There are \n{no_candidates}\ncandidates taking the\n{course.lower()}\ncourse.")
+
---
+++ ex042/3.py
@@ -0,0 +1,13 @@
+punishment_text = "WILL NOT TREACH OTHERS TO FLY\n"
+numb_of_repetition = 5
+
+# -- A --
+# print(punishment_text*numb_of_repetition)
+
+# -- B --
+# punishment_text = "WILL NOT TEACH OTHERS TO FLY\n"
+# numb_of_repetition = 5
+# print(punishment_text.replace('NOT','BAD')*numb_of_repetition)
+
+# -- c --
+print("The occurrence of the word THAT in the original sentence is: ",punishment_text.count('THAT'))
